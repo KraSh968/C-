ada8c8c6a9d32f9c096c33e2595bae8d48e30c3c
KraSh968 <94299177+KraSh968@users.noreply.github.com>
2022-04-05 00:08:40 +0400

    test2

---
+++ prak3/BdApp/BdApp/Models/OrderContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BdApp.Models
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
